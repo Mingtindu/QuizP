727070b8b01456b299aa5a349c318649553e08be
Mingtindu <mingmartu@gmail.com>
2024-04-21 17:17:14 +0545

    feat: User model created

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,16 @@
+import User from './model/user.model.js'
+
+const addUser = (){
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+    const {name,email,password} = req.body;
+
+
+}
---
+++ backend/src/model/user.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+
+    }
+
+})
+
+const User = mongoose.model('User',userSchema)
+
+
+export default User
