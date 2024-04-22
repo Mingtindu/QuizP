be7c47c97b5e68821ef3ecce210ef3d9a286244a
Mingtindu <mingmartu@gmail.com>
2024-04-22 07:28:45 +0545

    Feat: User registration controller added

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from './model/user.model.js'
+import mongoose from 'mongoose';
 
-const addUser = (){
+const addUser =async ((req,res)=>{
     // get user details from frontend
     // validation - not empty
     // check if user already exists: username, email
@@ -11,6 +12,33 @@ const addUser = (){
     // check for user creation
     // return res
     const {name,email,password} = req.body;
+    if (
+        [name, email, phone, password].some((field) => field?.trim() === "")
+    ) {
+        throw new Error(400, "All fields are required")
+    }
+    const exitedUser=User.findOne({email})})
+    if(exitedUser){
+        throw Error(409,"User with email already exist");
+    }
+
+    const user= await User.create({
+        name,
+        email,
+        password
+    })
+
+    const createdUser = await User.findByID(user._id).select(
+        "-password -refreshToken"
+
+    )
+    if(!createdUser){
+        throw Error(500,"Something went wrong while registering user")
+    }
+    return res.status(201).json({createdUser});
 
 
-}
+
+    
+
+export {addUser}
