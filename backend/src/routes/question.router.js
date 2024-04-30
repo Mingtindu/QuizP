b64a8f7acb58d84270b7c250d12d83d6cc422733
Mingtindu <mingmartu@gmail.com>
2024-04-30 10:08:47 +0545

    feat: LIttle change

--- backend/src/routes/question.router.js
+++ backend/src/routes/question.router.js
@@ -2,7 +2,6 @@ import { Router} from 'express'
 import { addQuestion,getQuestion } from '../controllers/question.controller.js'
 
 const router = Router()
-
 router.route('/addQuestion').post(addQuestion)
 router.route('/getQuestion').get(getQuestion)
 
