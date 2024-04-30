37a1182a77ac177d9cf33c2efebbcd541cd63020
Mingtindu <mingmartu@gmail.com>
2024-04-30 10:51:39 +0545

    feat: User icon added in nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Assuming you are using React Router for navigation
 import './Navbar.css'; // Import the CSS file for styling
-
+import nav_img from '../Images/quiz.jpg'
+import user_icon from '../Images/user.png'
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-brand">
-          <Link to="/" className="logo">YourLogo</Link>
+          <Link to="/dashboard" className="logo"><img src={nav_img} alt="" /></Link>
         </div>
         <div className="navbar-menu">
           <ul className="navbar-list">
@@ -24,7 +25,7 @@ const Navbar = () => {
             </li>
             <li className="navbar-item">
               <Link to="/user" className="navbar-link user-icon">
-                <i className="fas fa-user"></i>
+              <img src={user_icon} alt="" ></img>
               </Link>
             </li>
           </ul>
